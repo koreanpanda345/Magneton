d6136a91167f38b358cb3a57d30037808a304833
Cody Spratford <koreanpanda345@gmail.com>
2021-03-01 00:34:25 -0800

    Fixing the status

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -12,6 +12,6 @@ export class Ready implements IEvent {
 	invoke = async () => {
 		console.log(`${this._client.user?.username} is ready`);
 		this._client.user?.setStatus("dnd");
-		this._client.user?.setActivity(`${process.env.PREFIX}help | In ${this._client.guilds.cache.size}`);
+		this._client.user?.setActivity(`${process.env.PREFIX}help | In ${this._client.guilds.cache.size} servers`);
 	};
 }
